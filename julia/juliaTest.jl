41799af6af5bdf210d5db304f7d9ea331d7de605
Jason0411202 <Jason0411202@gmail.com>
2024-01-15 18:59:53 +0800

    juliaTest finish

--- julia/juliaTest.jl
+++ julia/juliaTest.jl
@@ -3,6 +3,7 @@ using JSON
 using Plots
 using Clustering
 using YAML
+using Statistics
 
 config = YAML.load_file("../parameter.yaml")
 
@@ -18,7 +19,6 @@ response = HTTP.get("http://localhost:8000?size=" * string(multiple))
 if response.status == 200
     # 解析 JSON 格式的回應
     points_str = String(response.body)
-    # points_str="[{\"x\":\"90.75090985203879\",\"y\":\"21.853916258697804\"}]"
     points = JSON.parse(points_str)
     points = JSON.parse(points)
 
@@ -30,49 +30,33 @@ if response.status == 200
     println("size of dx", size(dx)) # size of dx(142, 2)
 
 
-    # # 分k群並繪製圖表
-    # kmeans = kmeans(dx', CLUSTER_NUM; init = :kmpp, n_init = 10)
-    # new_dy = assignments(kmeans)
-    # centers = [mean(dx[findall(new_dy .== i), :], dims=1) for i in 1:CLUSTER_NUM]
+    # 計算 k-means
+    result = kmeans(dx', CLUSTER_NUM)
 
-    # plt = scatter(x_values, y_values, c = new_dy, markersize = 1)
+    # 繪製散佈圖
+    scatter(x_values, y_values, group=result.assignments, xlabel="X", ylabel="Y", title="K-means Clustering")
 
-    # # 標記每個群的平均值和標準差
-    # for i in 1:CLUSTER_NUM
-    #     cluster_points = dx[findall(new_dy .== i), :]
-    #     cluster_mean = mean(cluster_points, dims=1)
-    #     cluster_stddev = std(cluster_points, dims=1)
-    #     scatter!([centers[i][1]], [centers[i][2]], marker = (:x, 8, :red), label = "")
-    #     errorbar!(
-    #         [centers[i][1]],
-    #         [centers[i][2]],
-    #         xerr = cluster_stddev[1],
-    #         yerr = cluster_stddev[2],
-    #         marker = (:circle, 4, 0.2, :black),
-    #         label = "",
-    #     )
+    # 分別計算並印出每群的 mean stddev
+    labels = result.assignments
+    for i in 1:CLUSTER_NUM
+        cluster_points = dx[labels .== i, :]
+        cluster_mean = mean(cluster_points, dims=1)
+        cluster_stddev = std(cluster_points, dims=1)
 
-    #     # 顯示平均值和標準差的數值
-    #     plt = annotate!(
-    #         centers[i][1] + 0.1,
-    #         centers[i][2] + 0.1,
-    #         text("average: \($(round(cluster_mean[1], digits=2)), $(round(cluster_mean[2], digits=2)))\nstd: \($(round(cluster_stddev[1], digits=2)), $(round(cluster_stddev[2], digits=2)))", :blue),
-    #     )
-    # end
+        print("group $i means: ", cluster_mean)
+        println("  stddev: ", cluster_stddev)
+    end
 
-    # # 計時結束
-    # global end_time = time() - start
-    # println("time: ", end_time)
+    # 計時結束
+    global end_time = time() - start
+    println("time: ", end_time)
 
-    # if !isfile("./data/time.yaml")
-    #     touch("./data/time.yaml")
-    # end
+    if !isfile("./data/time.yaml")
+        touch("./data/time.yaml")
+    end
 
-    # data = YAML.load_file("./data/time.yaml") |> x -> x == nothing ? Dict() : x
-    # data["time_$(multiple)"] = end_time
-    # YAML.write("./data/time.yaml", data)
+    # YAML.write_file("./data/time.yaml", "time_" * string(NOW_MULTIPLE) * ": " * string(end_time))
 
-    # # savefig("./data/result_$(multiple).png")
-    # png("./data/result_$(multiple).png")
-    # plt
+    # 儲存圖片
+    savefig("./data/result_" * string(NOW_MULTIPLE) * ".png")
 end
