4eb833a721129fccad87606ade7f75352a2904a5
Jason0411202 <Jason0411202@gmail.com>
2024-01-22 08:36:21 +0800

    time analyze part finish

--- time_analyze/analyze.py
+++ time_analyze/analyze.py
@@ -1,27 +1,16 @@
 import yaml
 import matplotlib.pyplot as plt
 
-# 讀取第一個 time.yaml 檔案
-with open('../python/data/time.yaml', 'r') as file1:
-    data1 = yaml.safe_load(file1)
-
-# 讀取第二個 time.yaml 檔案
-with open('../R/data/time.yaml', 'r') as file2:
-    data2 = yaml.safe_load(file2)
-
-# 分離資料中的時間和數值
-times1, values1 = zip(*data1.items())
-times2, values2 = zip(*data2.items())
-
-# 將字串形式的數值轉換成浮點數
-values1 = [float(val) for val in values1]
-values2 = [float(val) for val in values2]
-
-# 繪製折線圖
+filePath=['../python/data/time.yaml','../R/data/time.yaml', '../Go/data/time.yaml', '../julia/data/time.yaml']
+language=['python','R', 'Go', 'julia']
 plt.figure(figsize=(10, 6))
 
-plt.plot(times1, values1, label='python')
-plt.plot(times2, values2, label='R')
+for i in range(len(filePath)):
+    with open(filePath[i], 'r') as file:
+        data = yaml.safe_load(file)
+    times, values = zip(*data.items())
+    values = [float(val) for val in values]
+    plt.plot(times, values, label=language[i])
 
 plt.title('time analyze')
 plt.xlabel('time')
